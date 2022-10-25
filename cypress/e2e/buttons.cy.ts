0f9dbd61428d80699bd652e8a4039b5359f63396
xeneas <xeneas@xeneas-laptop.local>
2022-10-25 08:17:56 -0400

    4to commit

--- cypress/e2e/buttons.cy.ts
+++ cypress/e2e/buttons.cy.ts
@@ -4,5 +4,10 @@ describe('Alerts', () => {
     })
     it('JS alerts', function(){
         cy.contains('button', 'Click for JS Alert').click();
+        cy.on('window:alert', message => {
+            expect(message).to.equal('I am a JS Alert')
+        });
+        cy.on('window:confirm', () => true);
+        cy.get('p#result').should('have.text', 'You successfully clicked an alert')
     })
 })
